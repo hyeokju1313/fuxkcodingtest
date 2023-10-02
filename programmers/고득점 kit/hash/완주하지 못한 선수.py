b00a3ba8da463b9d73e2d9932cd1f1c4ced85b4c
Lee Hyeokju <hyeokju1313@gmail.com>
2023-10-02 15:57:26 +0900

    Update 완주하지 못한 선수.py

--- programmers/고득점 kit/hash/완주하지 못한 선수.py
+++ programmers/고득점 kit/hash/완주하지 못한 선수.py
@@ -3,3 +3,10 @@ import collections
 def solution(participant, completion):
     left = collections.Counter(participant) - collections.Counter(completion)
     return list(left.keys())[0]
+
+
+# 중요 point
+# 1. collection 모듈과 Counter
+# 2. Counter(participant) - Counter(completion)
+# 3. dict keys() : dict(left)의 key를 return
+# 4. list(dict.keys())[0] : dict(left)의 key를 list로 변환
